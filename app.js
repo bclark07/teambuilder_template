20ddba562c4ce8996aeaa4f8afe24706b410b4b0
Brittany Clark <clark.brittany.j@gmail.com>
2020-01-26 08:12:23 -0500

    role js files updated

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+//check if need to add jest and remove path and fs, not in the package.json
+
+// The application will prompt the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns.
+
+// ### Roster output
+
+// The project must generate a `team.html` page in the `output` directory, that displays a nicely formatted team roster. Each team member should display the following in no particular order:
+
+//   * Name
+
+//   * Role
+
+//   * ID
+
+//   * Role-specific property (School, link to GitHub profile, or office number)
+
+// ## Bonus
+
+// * Use validation to ensure that the information provided is in the proper expected format.
+
+// * Add the application to your portfolio.
+
 inquirer
 
 
