507cc182afe9cf4d0beeb78ff83c8e12023a5267
Brittany Clark <clark.brittany.j@gmail.com>
2020-04-06 04:25:31 -0400

    storing data for each employee type

--- app.js
+++ app.js
@@ -1,120 +1,129 @@
+// const BuildHTML = require("./lib/BuildHTML");
+
+// // Initialize a new Game object
+// const build = new Build();
+
+// // Start playing
+// build.play();
+
 const inquirer = require("inquirer");
+const BuildHTML = require("./lib/BuildHTML");
+//add each individual file?
+const fs = require("fs"); //write BuildHTML file
+const EmployeeArr = []; //one mamager with employees
 // const path = require("path");
-const fs = require("fs");
+// const OUTPUT_DIR = path.resolve(_dirname, "output");
 
 //check if need to add jest and remove path and fs, not in the package.json
-
-// The application will prompt the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns.
 
 // ### Roster output
 
 // The project must generate a `team.html` page in the `output` directory, that displays a nicely formatted team roster. Each team member should display the following in no particular order:
 
-//   * Name
-
-//   * Role
-
-//   * ID
-
-//   * Role-specific property (School, link to GitHub profile, or office number)
-
 // ## Bonus
 
 // * Use validation to ensure that the information provided is in the proper expected format.
 
-// * Add the application to your portfolio.
-
-// const Employee = require("./lib/Employee");
-
-
-
-//ask if creating an engineer or intern, or putting team together
-// }
-//2 parameters - what file writing to, and 
-// use fs package
-// writeFile function
-
 // The project must generate a `team.html` page in the `output` directory, that displays a nicely formatted team roster. Each team member should display the following in no particular order:
 
-//   * Name
-
-//   * Role
-
-//   * ID
-
-//   * Role-specific property (School, link to GitHub profile, or office number)
-
-// function CreateTeam() {
-
 function CreateTeam() {
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "What role is the person you are creating the information for?",
-            name: "role",
-            choices: [
-                "Manager",
-                "Engineer",
-                "Intern"
-            ]
-        },
-        {
-            type: "input",
-            name: "name",
-            message: "What is the employee's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is the employee's id number?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is the employee's email address?"
-        }
+  //gets information that we need for all employees
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message:
+          "What role is the person you are creating the information for?",
+        name: "role",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the employee's id number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email address?",
+      },
     ])
-   .then(function (data) {
-    const role = data.role;
-    switch (role) {
-        case 'Manager':
-            inquirer.prompt([
-                {
-                    type: "input",
-                    name: "office",
-                    message: "What is the employee's office number?"
-                }
-            ]);
-            const Manager = require('./lib/Manager');
-            const boss = new Manager(data.name, data.id, data.email, data.office);
-            console.log(boss);
-            break;
-        case 'Engineer':
-            inquirer.prompt([
-                {
-                    type: "input",
-                    name: "github",
-                    message: "What is the employee's gitHub?"
-                }
-            ]);
-            const Engineer = require('./lib/Engineer');
-            console.log("Engineer");
-            const engine = new Engineer(data.name, data.id, data.email, data.github);
-            break;
-        case 'Intern':
-            inquirer.prompt([
-                {
-                    type: "input",
-                    name: "school",
-                    message: "Where does this intern go to school?"
-                }
-            ]);
-            const Intern = require('./lib/Intern');
-            const freeworker = new Intern(data.name, data.id, data.email, data.school);
-            console.log("Intern");
-    }
-});
-}
+    //gathers remaining information depending on the role and exports data to create employee card
+    .then(function (data) {
+      const role = data.role;
+      switch (role) {
+        case "Manager":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "office",
+                message: "What is the employee's office number?",
+              },
+            ])
+            .then(function () {
+              const Manager = require("./lib/Manager");
+              const boss = new Manager(
+                data.name,
+                data.id,
+                data.email,
+                data.office
+              );
+              console.log(boss);
+
+              // const html = Manager.generateManagerHTML(data.name, data.id, data.email, 100);
+              // return writeFileAsync("index.html", html);
+            });
+          break;
+        case "Engineer":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "github",
+                message: "What is the employee's gitHub?",
+              },
+            ])
+            .then(function () {
+              const Engineer = require("./lib/Engineer");
+
+              console.log("Engineer");
+              const engineer = new Engineer(
+                data.name,
+                data.id,
+                data.email,
+                data.github
+              );
+              //push engineer to the EngineerArr array, or teamMembers array (15 minutes into video from 2.15)
+            });
+          break;
+        case "Intern":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "school",
+                message: "Where does this intern go to school?",
+              },
+            ])
+            .then(function () {
+              const Intern = require("./lib/Intern");
+              const freeworker = new Intern(
+                data.name,
+                data.id,
+                data.email,
+                data.school
+              );
+              console.log("Intern");
+            });
+      }
+    });
+} //closes CreateTeam
 
 //   var filename = data.name.toLowerCase().split(' ').join('') + ".json";
 
@@ -127,3 +136,33 @@ function CreateTeam() {
 //     console.log("Success!");
 
 CreateTeam();
+
+//aync.await option:
+// const teamMembers = [];
+// const idArray = [];
+
+// // const teamMenu = teamMenu() => {
+//     function teamMenu() {
+
+//     const createTeam = async () => { //async function
+//         const response = await inquirer.prompt({
+//             {
+//                 type: "list",
+//                 message: "What role is the person you are creating the information for?",
+//                 name: "role",
+//                 choices: [
+//                     "Manager",
+//                     "Engineer",
+//                     "Intern"
+//                 ]
+//             }});
+// }
+
+//     //10 minutes into recording - how to populate question array with variables
+
+//     const { name, id, email, officeNumber } = response;
+//     const manager = new Manager(name, id, email, officeNumber);
+
+// }//closes teamMenu()
+
+// teamMenu();
